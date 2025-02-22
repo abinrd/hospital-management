72542f0a0e6e30c3fe020e66cee5e004580cfd96
Abinrd <abinrajuvilayil@gmail.com>
2025-02-22 23:35:03 +0530

    Server run

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,22 @@
 import {PORT} from './config/env.js'
+import ConnectDataBase from './database/mongodb.js';
+import express from 'express'
 
-const express=require("express")
 const app = express();
 
-app.get('/',(req,res)=>{
 
-})
+const StartServer =async()=>{
+    try{
+        await ConnectDataBase();
+        app.listen(PORT,()=>{
+            console.log(`🚀 Server running on http://localhost:${PORT}`);
+        })
+    }catch(error){
+        console.log("❌ Database connection failed",error);
+        process.exit(1);
+    }
+}
+
+StartServer();
+
+export default app ;
