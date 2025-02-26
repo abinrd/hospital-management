6a5373f737c2f3dbdd5005ae3eaa3fd9d0dfc1ee
Abinrd <abinrajuvilayil@gmail.com>
2025-02-26 16:36:00 +0530

    changes in authroute

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,7 @@
 import {Router} from 'express'
 import { login, register } from '../controller/auth.controller.js';
+import { inviteDoctor, verifyDoctorInvite, completeDoctorRegistration, createFirstAdmin } from '../controller/auth.controller.js';
+import { authorize, isAdmin } from '../middleware/auth.middleware.js';
 
 
 const authRouter = Router();
@@ -7,6 +9,10 @@ const authRouter = Router();
 //Path:api/v1/auth/login||register(POST)
 authRouter.post('/register',register)
 authRouter.post('/login',login)
+authRouter.post('/invite-doctor', authorize, isAdmin, inviteDoctor);
+authRouter.get('/verify-doctor-invite/:token', verifyDoctorInvite);
+authRouter.post('/complete-doctor-registration', completeDoctorRegistration);
+authRouter.post('/create-first-admin', createFirstAdmin);
 
 
 export default authRouter;
