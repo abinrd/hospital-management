f5cabf4a66b62d6763db0c399c9cef17809692ae
Abinrd <abinrajuvilayil@gmail.com>
2025-02-26 16:36:53 +0530

    Changes in userroute

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,20 +1,18 @@
 import { Router } from "express";
-import { getUserProfile,updateUserProfile,getAllUsers,deleteUser } from "../controller/user.controller.js";
+import { getUserProfile,updateUserProfile,getAllUsers,deleteUser,approveDoctor,promoteToAdmin,getPendingDoctors,getApprovedDoctors } from "../controller/user.controller.js";
 import { authorize } from "../middleware/auth.middleware.js";
 import roleAuthorize from "../middleware/role.middleware.js";
 
 const userRouter = Router();
 
-// Get logged-in user's profile (Patients & Doctors)
-userRouter.get("/profile", authorize, getUserProfile);
 
-// Update logged-in user's profile (Patients & Doctors)
-userRouter.put("/profile", authorize, updateUserProfile);
-
-// Get all users (Admin only)
-userRouter.get("/", authorize, roleAuthorize(["admin"]), getAllUsers);
-
-// Delete a user (Admin only)
-userRouter.delete("/:id", authorize, roleAuthorize(["admin"]), deleteUser);
+userRouter.get("/profile", authorize, getUserProfile);// Get logged-in user's profile (Patients & Doctors)
+userRouter.put("/profile", authorize, updateUserProfile);// Update logged-in user's profile (Patients & Doctors)
+userRouter.get("/", authorize, roleAuthorize(["admin"]), getAllUsers);// Get all users (Admin only)
+userRouter.delete("/:id", authorize, roleAuthorize(["admin"]), deleteUser);// Delete a user (Admin only)
+userRouter.patch('/approve-doctor/:doctorId', authorize, roleAuthorize(['Admin']), approveDoctor);
+userRouter.patch('/promote-to-admin/:userId', authorize, roleAuthorize(['Admin']), promoteToAdmin);
+userRouter.get('/pending-doctors', authorize, roleAuthorize(['Admin']), getPendingDoctors);
+userRouter.get('/approved-doctors', authorize, getApprovedDoctors);
 
 export default userRouter;
